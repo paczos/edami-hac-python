47678331fa1d9b013bf6795a5312a13dfcbeb4c4
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-12 19:17:07 +0200

    code aesthetics

--- kmeans.r
+++ kmeans.r
@@ -1,20 +1,21 @@
 library("tidyverse")
 
 dist <- function(a, b){
-    #    print(paste(a, b, sqrt(sum((a - b) ^ 2))))
     sqrt(sum((a - b) ^ 2))
 }
 
 kmeans <- function(data, groupcount)
 {
-    # using random points from datasets as initial positions for centroids
+    # convergence threshold -> what is the maximal number of items that can be moved between clusters, so that kmeans can stop now
+    convTh <- 2
 
+    # using random points from datasets as initial positions for centroids
     centroids <- data[sample(nrow(data), groupcount),];
     clusterIds <- integer(nrow(data));
 
     while (TRUE) {
 
-        modifiedCluster = 0
+        modifiedClusterCount <- 0
         # calculate clusters by finding closest centroid for each point
         for (i in 1 : nrow(data)) {
             row <- data[i,];
@@ -23,36 +24,30 @@ kmeans <- function(data, groupcount)
             })
 
             # store closest centroid
-            #print(distances)
-            newClusterId = which.min(distances);
-            #print(distances)
-            #print(paste(clusterIds[i], newClusterId));
-
-            #Error in if (clusterIds[i] != newClusterId) { :
-            # argument is of length zero
+            newClusterId <- which.min(distances);
 
             if (clusterIds[i] != newClusterId) {
                 clusterIds[i] <- newClusterId;
-                modifiedCluster = modifiedCluster + 1 ;
+                modifiedClusterCount <- modifiedClusterCount + 1 ;
             }
         }
 
-        if (modifiedCluster < 2)
+        if (modifiedClusterCount < convTh)
         {
-            # if we got here, no item was moved between clusters -> the algorithm had converged
+            # if we got here, less than convTh items were moved between clusters -> the algorithm had converged
             break
         }
         else {
-            print(paste("modified ", modifiedCluster))
+            print(paste("modified ", modifiedClusterCount))
         }
 
         # calculate new centroids
         for (i in 1 : nrow(centroids)) {
-            clusterItems = data[clusterIds == i,];
-            centroids[i, ] = colMeans(clusterItems);
+            clusterItems <- data[clusterIds == i,];
+            centroids[i,] <- colMeans(clusterItems);
         }
     }
-    data$clusterId = clusterIds;
+    data$clusterId <- clusterIds;
     data
 }
 
