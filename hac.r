69fe4091ec6dbb1d66e1cc3d44109d5bcec1cef1
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-15 17:48:43 +0200

    fixed typo :)

--- hac.r
+++ hac.r
@@ -1,3 +1,9 @@
-hac <- function(data, coohesion){
-     0
+hac <- function (data, coohesion)
+{
+# initially: treating each point as a separate cluster
+# using centroids to asses distances between clusters
+# at each step linking clusters that are closest to each other
+for i in nrow(data) {
+
+}
 }
