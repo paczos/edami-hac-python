c80e788b74f295df0b9b95faadfe48d940b20d5f
Pawel Paczuski <pawel.paczuski@upmedic.pl>
2019-04-15 20:03:57 +0200

    find best pair for link

--- hac.r
+++ hac.r
@@ -1,9 +1,31 @@
+dist <- function(a, b){
+    sqrt(sum((a - b) ^ 2))
+}
+
 hac <- function (data, coohesion)
 {
-# initially: treating each point as a separate cluster
-# using centroids to asses distances between clusters
-# at each step linking clusters that are closest to each other
-for i in nrow(data) {
+    # initially: treating each point as a separate cluster
+    # using centroids to asses distances between clusters
+    # at each step linking clusters that are closest to each other
 
-}
+    clusterCandidates <- data;
+    connections = []
+    # find closest pair
+    s <- combn(1 : nrow(clusterCandidates), 2, function(pair){
+        fst = pair[1];
+        snd = pair[2];
+        distance <- dist(clusterCandidates[fst,], clusterCandidates[snd,]);
+        c(fst, snd, distance);
+    });
+    print(s[1,])
+    minimalVecIdx <- which.min(s[3,])
+
+    fst <- s[1, minimalVecIdx];
+    snd <- s[2, minimalVecIdx];
+    distance <- s[3, minimalVecIdx];
+    print(paste("idx:", minimalVecIdx, fst, snd, distance));
+
+
+
+    # should there be another link placed?
 }
